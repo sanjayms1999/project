e9bc4b0cb705f5a7e4873871b9c9db618d5dc201
sanjayms1999 <53893886+sanjayms1999@users.noreply.github.com>
2019-11-18 19:19:38 +0530

    Create precrip.py

---
+++ precrip.py
@@ -0,0 +1,39 @@
+from tkinter import *
+ 
+window = Tk()
+ 
+window.title("Health Monitoring system")
+ 
+window.geometry('1024x800')
+
+Label(window, text="PRESRIP",font="arial 15 bold").pack()
+
+l1=Label(window,text="Med Name")
+l1.place(x=150,y=150)
+
+e1=Entry()
+e1.place(x=230,y=150)
+
+
+l2=Label(window,text="MG")
+l2.place(x=150,y=200)
+
+e2=Entry()
+e2.place(x=230,y=200)
+
+l3=Label(window,text="Timings")
+l3.place(x=150,y=250)
+
+e3=Entry()
+e3.place(x=230,y=250)
+
+
+
+b1=Button(window,text="Submit",width=20,height=3,bd=10)
+b1.place(x=150,y=400)
+
+b2=Button(window,text="Log out",width=20,height=3,bd=10)
+b2.place(x=750,y=400)
+#b3 = Button(window, text="Back",width=20, height=3,bd=10,command=log_in).pack()
+
+window.mainloop()
